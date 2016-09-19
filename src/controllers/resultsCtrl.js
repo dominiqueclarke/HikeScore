d3e1a1e4dac64937734da24df61db3d51f9c1e86
Dominique Clarke <dominiquedclarke@gmail.com>
2016-09-19 14:33:56 -0500

    fixed bug with location disappearing

--- src/controllers/resultsCtrl.js
+++ src/controllers/resultsCtrl.js
@@ -3,7 +3,7 @@ angular.module('HikeScore')
     $scope.rating = $stateParams.rating;
     $scope.zip = $stateParams.zip;
     $scope.geoData = $stateParams.geoData;
-    $scope.city = $scope.geoData.address;
+    $scope.city = $stateParams.address;
     $scope.geoData.address = "";
     $scope.places = $stateParams.places;
     $scope.activities = $stateParams.activities;
